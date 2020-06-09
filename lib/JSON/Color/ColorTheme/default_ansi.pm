382d7e899fa042817010dbee3590e48efe944783
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-09 16:48:01 +0700

    Add color theme support

--- lib/JSON/Color/ColorTheme/default_ansi.pm
+++ lib/JSON/Color/ColorTheme/default_ansi.pm
@@ -1,4 +1,4 @@
-package JSON::Color::ColorTheme::default;
+package JSON::Color::ColorTheme::default_ansi;
 
 # AUTHORITY
 # DATE
@@ -10,16 +10,18 @@ use Term::ANSIColor qw(:constants);
 
 our %THEME = (
     v => 2,
-    summary => 'The default color theme for JSON::Color',
-    colors => {
+    summary => 'The default color theme for JSON::Color, using ANSI codes',
+    items => {
         string_quote         => {ansi_fg=>BOLD . BRIGHT_GREEN},
         string               => {ansi_fg=>GREEN},
         string_escape        => {ansi_fg=>BOLD},
         number               => {ansi_fg=>BOLD . BRIGHT_MAGENTA},
         true                 => {ansi_fg=>BOLD . CYAN},
         false                => {ansi_fg=>CYAN},
+        bool                 => {ansi_fg=>CYAN},
         null                 => {ansi_fg=>BOLD . BLUE},
         object_key           => {ansi_fg=>MAGENTA},
+        object_key_quote     => {ansi_fg=>MAGENTA},
         object_key_escape    => {ansi_fg=>BOLD},
         linum                => {ansi_fg=>REVERSE . WHITE},
     },
---
+++ lib/JSON/Color/ColorTheme/default_rgb.pm
@@ -0,0 +1,32 @@
+package JSON::Color::ColorTheme::default_rgb;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use parent 'ColorThemeBase::Static::FromStructColors';
+use Graphics::ColorNamesLite::WWW;
+
+my $t = $Graphics::ColorNamesLite::WWW::NAMES_RGB_TABLE;
+
+our %THEME = (
+    v => 2,
+    summary => 'The default color theme for JSON::Color, using RGB color codes',
+    items => {
+        string_quote         => $t->{forestgreen},
+        string               => $t->{green},
+        string_escape        => $t->{greenyellow},
+        number               => $t->{darkmagenta},
+        true                 => $t->{cyan},
+        false                => $t->{darkcyan},
+        null                 => $t->{blue},
+        object_key           => $t->{darkmagenta},
+        object_key_quote     => $t->{darkmagenta},
+        object_key_escape    => $t->{magenta},
+        linum                => {bg=>$t->{white}, fg=>$t->{black}},
+    },
+);
+
+1;
+# ABSTRACT:
